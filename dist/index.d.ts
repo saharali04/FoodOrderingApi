995fef6c56eb7882e0abe7d9bc058717e537d058
Sahar Ali <saharnazimali@gmail.com>
2021-09-10 09:28:37 -0400

    basic Loopback4 setup

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, FoodOrderingApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<FoodOrderingApplication>;
